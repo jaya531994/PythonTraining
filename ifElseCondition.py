94549cc5cc22ed8e0d090f05eaee177aab682864
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2022-05-17 22:19:46 +0530

    Add files via upload

---
+++ ifElseCondition.py
@@ -0,0 +1,99 @@
+
+#-----------------------------------------------------
+#if syntax
+# ----------------------------------------------------
+# if expression:
+#     statements
+
+#Ex-1
+
+# a =500
+# b =100
+
+# if b > a:
+#     print("b is greater than a")
+# if b < a:
+#     print("b is less than a")
+
+#Ex-2
+# a = int(input("Enter a number"))
+# if a%2 ==0:
+#     print("number is even")
+
+# Ex-3
+# a = int(input("Enter a number"))
+# b = int(input("Enter a number"))
+# c = int(input("Enter a number"))
+# if a>b and a >c :
+#     print("a is larger")
+# if b>a and b >c:
+#     print("b is larger")
+# if c>b and c >a:
+#     print("c is larger")
+
+#nested if
+#-------------------------------
+#Ex-4 check number is even or odd
+# a =31
+# if a%2 == 0:
+#     if a < 10:
+#         print("Number is Even less then 10")
+#     else:
+#         print("Number is Even and greater then 10")
+
+
+# ----------------------------------------------------
+# indentation
+# ----------------------------------------------------
+# a = int(input("Enter a number"))
+# if a%2 ==0:
+# print("number is even")
+
+# ----------------------------------------------------
+# Syntax of else condition 
+# ----------------------------------------------------
+# if condition:  
+#     #block of statements   
+# else:   
+#     #another block of statements (else-block)  
+
+# Ex –1  Program to check whether a number is even or not.
+# a =int(input("Enter a number"))
+# if a%2 == 0:
+#     print("Number is Even")
+# else:
+#     print("Number is odd")
+
+# Ex-2 : Program to check whether a person is eligible to vote or not
+# age = int(input("Enter a number"))
+# if age >=18:
+#     print("person is eligible to vote")
+# else:
+#     print("person is not eligible to vote")
+
+
+# ----------------------------------------------------
+# The syntax of the elif statement is given below.
+# ----------------------------------------------------
+# if expression 1:   
+#     # block of statements   
+# elif expression 2:   
+#     # block of statements   
+# elif expression 3:   
+#     # block of statements   
+# else:   
+#     # block of statements 
+
+
+# Ex - 
+# a =33
+# b=100
+# c =150
+# if a>b and a >c :
+#     print("a is larger")
+# elif b>a and b >c:
+#     print("b is larger")
+# elif c>b and c >a:
+#     print("c is larger")
+
+
