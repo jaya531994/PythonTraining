1f53f337172e3a6aa6118f6d49d9077ff530354a
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2022-07-12 21:18:52 +0530

    Add files via upload

--- listmethods.py
+++ listmethods.py
@@ -10,18 +10,21 @@
 # 3.extend() - thi method will add multiple elemets at a time in the last index of list.
 
 #append()
+#--------------------------------------------------------------
 # l1.append(30)
 # print(l1)
 # l1.append(30)
 # print(l1)
 
 #index()
+#--------------------------------------------------------------
 # l1.insert(2,40)
 # print(l1)
 # l1.insert(3,50)
 # print(l1)
 
 #extend()
+#--------------------------------------------------------------
 
 # l1.extend(['x','y','z'])
 # print(l1)
@@ -53,14 +56,71 @@
 
 #count
 #-----------------------
-l1 = [10,20,30,40,50,10,10,30,'a']
+# l1 = [10,20,30,40,50,10,10,30,'a']
 # print(l1.count(10))
 
 # print(len(l1))
 
 #index()
+#--------------------------------------------------------------
 # print(l1.index('a'))
-print(min(l1))
+# print(min(l1))
+
+#max
+#--------------------------------------------------------------
+# from filecmp import cmp
+
+
+# l1 = [10,20,30,40,50,10,10,30]
+# l2 = set(l1)
+# print(l2)
+# l2 = max(l1)
+# print(l2)
+
+#min
+#--------------------------------------------------------------
+# l1 = 50
+# l2 = 10
+# print(min(l1))
+
+#compare
+#--------------------------------------------------------------
+# print(cmp(l1,l2))
+
+#index
+#------------------------
+l1 = [10,20,30,40,50,10,10,30,50]
+# print(l1.index(50))
+
+# pop - used to delete elements from the list based on index
+#--------------------------------------------------------------
+# print(l1.pop()) - by default it will delete last element from the list
+# print(l1.pop(1)) - This will delete index based element from the list
+# print(l1)
+
+#remove - used to delete elements from the list based on elements
+#-------------------------------------------------------------------
+# print(l1.remove(20)) 
+# print(l1)
+
+#Reverse - this method will reverse a given list
+# print(l1.reverse())
+# print(l1)
+
+#sort - 
+# l1 = ['a','abc','ac','ab']
+l1.sort() 
+# - By Default it will sort in ascending order
+l1.sort(reverse=True)
+#  - If you want to sort in descending order we have pass reverse = True
+print(l1)
+
+
+
+
+
+
+
 
 
 
