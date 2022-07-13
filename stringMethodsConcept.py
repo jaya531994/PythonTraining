0beeb07b98e6a3169d66f1fd4ce4dd44369934f9
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2022-07-13 22:08:43 +0530

    Add files via upload

---
+++ stringMethodsConcept.py
@@ -0,0 +1,26 @@
+from zlib import Z_PARTIAL_FLUSH
+
+
+l1 = [10,20,30,'a']
+# print(l1[2])
+
+#interview question
+#Can we update elements of string?
+
+# var1 ="Hello World"
+# # var1[0] = 'P'
+# print(var1[:6] + 'python')
+
+
+# str1 = "Therefore, ' ' this raises an exception that the immutable objects do not change their value but the value of constituents changes their value."
+# print(str1)
+
+# print("r \n  print \n and print R'\n' print \n")
+
+#String Formating
+name = 'jaya'
+weight = 21
+print("My name is %s and weight is %d kg!" % (name, weight))
+
+
+
