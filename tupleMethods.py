8b850b30ae245864913ad10c7d7bf2c2b2b427fb
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2022-07-13 20:33:34 +0530

    Add files via upload

---
+++ tupleMethods.py
@@ -0,0 +1,76 @@
+# tup1 = ('physics', 'chemistry', 1997, 2000)
+# print(tup1)
+# tup2 = (1,2,3,4)
+# print(tup2)
+# tup3 = "a", "b", "c", "d"
+# print(type(tup3))
+# tup4 = 1, 2, 3, 4
+# print(type(tup4))
+# tup5 = (50,)
+# print('using , it will return tuple',tup5)
+# tup5 = (50) 
+# print('this will return interger value',tup5)
+
+#Updating Tuple 
+# li1 = ['physics', 'chemistry', 1997, 2000]
+# li1[0] = 'math'
+# print(li1)
+# tup1 = ('physics', 'chemistry', 1997, 2000)
+# tup1[0] = 'math'
+# print(tup1)
+
+#add two tuples
+# tup1 = ('physics', 'chemistry', 1997, 2000)
+# tup2 = (1,2,3,4)
+# print(tup1+tup2)
+
+# tup1 = ('physics', 'chemistry', 1997, 2000)
+# tup2 = 12,
+# print(tup1+tup2)
+
+
+#delete tuple
+# tup1 = ('physics', 'chemistry', 1997, 2000)
+# print(tup1)
+# del(tup1)
+# print(tup1)
+
+# print('abc', -4.24e93, 18+6.6j, 'xyz')
+
+# a =10
+# b =20
+
+# a,b,c =10,20,30
+# print("value of a,b,c",a,b,c)
+
+#Tuple Methods
+#all()
+# tup1 = ('physics', 'chemistry', 1997, 2000 , True, False)
+# # tup1 = ()
+# print(all(tup1))
+
+#any
+# tup1 = ('physics', 'chemistry', 1997, 2000 , False)
+# tup1 = ()
+# print(any(tup1))
+
+#enumerate
+# tup1 = ('physics', 'chemistry', 1997, 2000 , False)
+# print(tuple(enumerate(tup1)))
+# print(tuple(enumerate(tup1,2)))
+
+#sum()
+# tup1 = ('physics', 'chemistry', 1997, 2000 , False)
+# print(sum(tup1))
+
+# tup1 = (1997, 2000)
+# print(sum(tup1))
+
+#sorted
+# tup1 = ('physics', 'chemistry', 1997, 2000 , False)
+# print(sorted(tup1))
+
+# tup1 = (10,20,40,50,2,5)
+# print(sorted(tup1))
+
+
