0347e55551e34c06230ce34b44dc3fcfbd04c74f
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2022-09-10 19:56:02 +0530

    Add files via upload

---
+++ importtest.py
@@ -0,0 +1,16 @@
+# import module.moduleExample
+#rename
+# import module.moduleExample as abc
+from moduleExample import square,ABC
+# gettng all function using *
+
+
+res = square(5)
+print(res)
+
+ad = ABC(10,20)
+print(ad)
+
+def Seprate(x,y):
+    c = x- y
+    return c
