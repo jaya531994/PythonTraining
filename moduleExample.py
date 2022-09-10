8e790bdd8c0f5d65798677c19d0908d5b44b6abd
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2022-09-10 19:52:56 +0530

    Add files via upload

---
+++ moduleExample.py
@@ -0,0 +1,20 @@
+import math
+# import datetime as abc
+import imp
+
+# print( "The value of euler's number is", math.e )
+# print(abc.datetime.now())
+
+def square( number ):
+    """This function will square the number passed to it"""
+    result = number ** 2
+    return result
+
+def ABC(a,b):
+    c = a+b
+    return c
+
+# obj = square(10)
+# print(obj)
+
+
